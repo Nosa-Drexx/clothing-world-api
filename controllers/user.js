cf6c809be38289d575522e9349633d55baa24f47
Egharevba Nosa <96873980+Nosa-Drexx@users.noreply.github.com>
2024-08-01 21:17:35 +0100

    added api for to update add to and remove products from user cart

--- controllers/user.js
+++ controllers/user.js
@@ -12,3 +12,147 @@ export const userExist = async (req, res) => {
     return res.status(400).json({ success: false, error: e.message });
   }
 };
+
+export const addProductToCart = async (req, res) => {
+  const { _id } = req.user;
+  const { productId, amount } = req.body;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Check if the product is already in the cart
+    const existingCartItem = user.cart.find((item) =>
+      item.productId.equals(productId)
+    );
+
+    if (existingCartItem) {
+      return res.status(400).json({
+        success: false,
+        message: "Product already in cart",
+      });
+    }
+
+    const userUpdated = await User.findByIdAndUpdate(
+      _id,
+      {
+        $push: {
+          cart: {
+            productId,
+            amount: amount > 0 ? amount : 1,
+          },
+        },
+      },
+      { new: true } // This option returns the updated document
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Product added to cart",
+      data: userUpdated,
+    });
+  } catch (e) {
+    return res.status(400).json({ success: false, error: e.message });
+  }
+};
+
+export const removeProductFromCart = async (req, res) => {
+  const { _id } = req.user;
+  const { productId } = req.body;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const existingCartItem = user.cart.find((item) =>
+      item.productId.equals(productId)
+    );
+
+    if (!existingCartItem) {
+      return res.status(400).json({
+        success: false,
+        message: "Product does not exist in cart",
+      });
+    }
+
+    const userUpdated = await User.findByIdAndUpdate(
+      _id,
+      {
+        $pull: {
+          cart: {
+            productId,
+          },
+        },
+      },
+      { new: true } // This option returns the updated document
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Product removed from cart",
+      data: userUpdated,
+    });
+  } catch (e) {
+    return res.status(400).json({ success: false, error: e.message });
+  }
+};
+
+export const updateProductAmount = async (req, res) => {
+  const { _id } = req.user;
+  const { productId, amount } = req.body;
+
+  try {
+    if (amount <= 0) return removeProductFromCart(req, res); // Remove product if amount is less than or equal to 0
+
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const existingCartItem = user.cart.find((item) =>
+      item.productId.equals(productId)
+    );
+
+    if (!existingCartItem) {
+      return res.status(400).json({
+        success: false,
+        message: "Product does not exist in cart",
+      });
+    }
+
+    const userUpdated = await User.findByIdAndUpdate(
+      _id,
+      {
+        $set: {
+          "cart.$[element].amount": amount > 0 ? amount : 1,
+        },
+      },
+      {
+        arrayFilters: [{ "element.productId": productId }],
+        new: true, // This option returns the updated document
+      }
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Product amount updated",
+      data: userUpdated,
+    });
+  } catch (e) {
+    return res.status(400).json({ success: false, error: e.message });
+  }
+};
+
+export const getCart = async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findById(_id).populate({ path: "cart.productId" });
+    return res.status(200).json({ success: true, data: user.cart });
+  } catch (e) {
+    return res.status(400).json({ success: false, error: e.message });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { userExist } from "../controllers/user.js";
+import {
+  addProductToCart,
+  getCart,
+  removeProductFromCart,
+  updateProductAmount,
+  userExist,
+} from "../controllers/user.js";
+import { requireLogin } from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/user-exist/:username", userExist);
+router.put("/user/cart/add", requireLogin, addProductToCart);
+router.delete("/user/cart/remove", requireLogin, removeProductFromCart);
+router.put("/user/cart/update", requireLogin, updateProductAmount);
+router.get("/user/cart", requireLogin, getCart);
 
 export { router };
